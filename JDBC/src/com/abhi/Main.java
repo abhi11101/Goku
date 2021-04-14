a09f4e9099970e24315389db54a1e501eb7c5779
Abhijeet Singh <64970406+abhi11101@users.noreply.github.com>
2021-04-14 20:23:43 +0530

    Intellij Commit with Main update...

--- JDBC/src/com/abhi/Main.java
+++ JDBC/src/com/abhi/Main.java
@@ -7,15 +7,16 @@ import java.sql.Statement;
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-		String query ="select * from books order by book_id";
-		Connection con = ConnectorClass.connectIt();
-		Statement st = con.createStatement();
-		ResultSet rs = st.executeQuery(query);
-		String data="";
-		while(rs.next()) {
-			data = rs.getString(1) + " " + rs.getString(2);	
-			System.out.println(data);
-		}
+//		String query ="select * from books order by book_id";
+//		Connection con = ConnectorClass.connectIt();
+//		Statement st = con.createStatement();
+//		ResultSet rs = st.executeQuery(query);
+//		String data="";
+//		while(rs.next()) {
+//			data = rs.getString(1) + " " + rs.getString(2);
+//			System.out.println(data);
+//		}
+		System.out.println("Intellij Update...");
 		
 
 	}
